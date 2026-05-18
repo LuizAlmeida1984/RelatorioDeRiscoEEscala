84add67be2998c6d301a0e955c2c703008f0129a
Vinicius Moreira <vmarv@hotmail.com>
2026-05-18 15:16:23 -0300

    Ajuste para envio a VPS

---
+++ backend/bootstrap/cache/packages.php
@@ -0,0 +1,37 @@
+<?php return array (
+  'google-gemini-php/laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Gemini\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
